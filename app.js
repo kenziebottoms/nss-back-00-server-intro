248fd445b75532235d0c75431df4b1d931f38184
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-13 13:25:57 -0500

    html into public folder, add express.static phrase

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 
-const logParams = (req, res, next) => {
+const logParams = (err, req, res, next) => {
     console.log("this is middleware");
     console.log("req.params", req.params.id);
     console.log("req.url", req.url);
+    if (err) next(err);
     next();
 };
 
 // runs on all routes
+app.use(express.static(__dirname+"/public"));
 app.use(logParams);
 
 // runs only on localhost:8080/
@@ -19,6 +21,16 @@ app.get("/", (req, res) => {
 // runs only on /yo
 app.get("/yo", (req, res) => {
     res.send("Yo world");
+});
+
+app.use((req, res, next) => {
+    let err = new Error("Not found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.send(`oh shit: ${err}`);
 });
 
 app.listen(8080, () => {
