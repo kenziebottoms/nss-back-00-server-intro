d6b0f1c9d012d158417bfbf1f48265139d5b349e
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-13 11:44:18 -0500

    added another route, comments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 A practice code-along to see what we're missing without Express.
+
+| Request      | <-->         | Response      |
+| ------------ | ------------ | ------------- |
+| `readStream` | `transform`  | `writeStream` |
+| `get()`      | `use()`      | `send()`      |
+|              | (middleware) |               |
--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
 
+const logParams = (req, res, next) => {
+    console.log("this is middleware");
+    console.log("req.params", req.params.id);
+    console.log("req.url", req.url);
+    next();
+};
+
+// runs on all routes
+app.use(logParams);
+
+// runs only on localhost:8080/
 app.get("/", (req, res) => {
     res.send("Hello world");
+});
+
+// runs only on /yo
+app.get("/yo", (req, res) => {
+    res.send("Yo world");
 });
 
 app.listen(8080, () => {
