048b30acfc5829cbfe379e21bd8b0b162ede0291
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-13 10:13:13 -0500

    added index.html with readstream

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const server = createServer();
 server.on("request", (req, res) => {
     console.log("req method", req.method);
     console.log("req URL", req.url);
-    // console.log("req headers", req.headers);
+    res.writeHead(200, {"useless-message": "yo bitch"});
+    readFile("./index.html", (err, file) => {
+        if (err) {
+            res.statusCode = 404;
+            res.end();
+        } else {
+            res.end(file);
+        }
+    });
 });
 
 server.listen(8080, () => {
