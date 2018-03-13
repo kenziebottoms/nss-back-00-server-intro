6bcf365a224b7fefe057a9645351a6ed51a4c158
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-13 10:03:56 -0500

    install nodemon

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const { createServer } = require("http");
 const server = createServer();
 
 server.on("request", (req, res) => {
-    // console.log("req method", req.method);
-    // console.log("req URL", req.url);
+    console.log("req method", req.method);
+    console.log("req URL", req.url);
     // console.log("req headers", req.headers);
-    console.log("req", req);
 });
 
 server.listen(8080, () => {
