bdd9c8a1314667977fa504d5b17d7873a654c1a3
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-13 10:40:09 -0500

    dynamic html path

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const { createServer } = require("http");
 const server = createServer();
 
 server.on("request", (req, res) => {
-    console.log("req method", req.method);
-    console.log("req URL", req.url);
     res.writeHead(200, {"useless-message": "yo bitch"});
-    readFile("./index.html", (err, file) => {
+    
+    let url = req.url;
+    let resource = url.slice(-1) === "/" ? url.slice(1).concat("index.html") : `${url.slice(1)}.html`;
+    readFile(resource, (err, file) => {
         if (err) {
             res.statusCode = 404;
-            res.end();
+            res.end("Not found");
         } else {
             res.end(file);
         }
     });
 });
+
 
 server.listen(8080, () => {
     console.log("listening on port 8080");
