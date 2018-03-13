e80f36a16fdce659941f885b3980daa33fe52efe
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-13 11:10:41 -0500

    express

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-const { readFile } = require("fs");
-const { createServer } = require("http");
+const express = require("express");
+const app = express();
 
-const server = createServer();
-
-server.on("request", (req, res) => {
-    let url = req.url;
-    let resource = url.slice(-1) === "/" ? url.slice(1).concat("index.html") : `${url.slice(1)}.html`;
-    readFile(resource, (err, file) => {
-        if (err) {
-            res.statusCode = 404;
-            res.end("Not found");
-        } else {
-            res.end(file);
-        }
-    });
+app.get("/", (req, res) => {
+    res.send("Hello world");
 });
 
-
-server.listen(8080, () => {
-    console.log("listening on port 8080");
+app.listen(8080, () => {
+    console.log("Listening on port 8080");
 });
