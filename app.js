30753ece535a811e259ad713df31d77dcf4f8be0
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-13 10:50:31 -0500

    extra html page for routing

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const { createServer } = require("http");
 const server = createServer();
 
 server.on("request", (req, res) => {
-    res.writeHead(200, {"useless-message": "yo bitch"});
-    
     let url = req.url;
     let resource = url.slice(-1) === "/" ? url.slice(1).concat("index.html") : `${url.slice(1)}.html`;
     readFile(resource, (err, file) => {
